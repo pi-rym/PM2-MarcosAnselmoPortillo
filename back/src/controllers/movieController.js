ec6cfa21da7c2d499544c96a339cd71c47650a7a
MarcosAnselmoPortillo <marcosportillo111@gmail.com>
2024-03-13 20:38:15 -0300

    Adding movieService. H8 Express II - M2

--- back/src/controllers/movieController.js
+++ back/src/controllers/movieController.js
@@ -1,5 +1,16 @@
+const movieService = require("../services/movieService");
+
 module.exports = {
-  getAllMovies: (req, res) => {
-    res.status(200).send("Proximamente estarán todas las películas");
+  getAllMovies: async (req, res) => {
+    try {
+      const movies = await movieService.getMovies();
+      res.status(200).json(movies);
+      console.log(res);
+      
+    } catch (error) {
+      res.status(500).json({
+        error: "Error interno del servidor",
+      })
+    }
   },
 };
