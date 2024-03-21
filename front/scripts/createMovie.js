01fcc532e219f7f59e610767d1cb28d53cdea103
MarcosAnselmoPortillo <marcosportillo111@gmail.com>
2024-03-21 11:17:07 -0300

    Creating POST to backend. H12-13-14 - Activity 3 - M2

--- front/scripts/createMovie.js
+++ front/scripts/createMovie.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 function limpiarFormulario() {
   document.getElementById("movieForm").reset();
 }
@@ -8,15 +10,33 @@ function enviarFormulario(event) {
   const year = document.getElementById("year").value;
   const director = document.getElementById("director").value;
   const duration = document.getElementById("duration").value;
-  const genres = Array.from(document.getElementById("genre").selectedOptions).map(option => option.value);
+  const genres = Array.from(
+    document.getElementById("genre").selectedOptions
+  ).map((option) => option.value);
   const rate = document.getElementById("rate").value;
   const poster = document.getElementById("poster").value;
 
+  const requestBody = {
+    title: title,
+    year: parseInt(year),
+    director: director,
+    duration: duration,
+    genre: genres,
+    rate: parseFloat(rate),
+    poster: poster,
+  };
+  
   if (title && year && director && duration && genres && rate && poster) {
-    //console.log(title, year, director, duration, genre, rate, poster);
-    //
-    alert("Formulario enviado correctamente");
-    limpiarFormulario();
+    axios
+      .post("http://localhost:3000/movies", requestBody)
+      .then(() => {
+        alert("Película agregada correctamente");
+        limpiarFormulario();
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Error agregando película");
+      });
   } else {
     alert("Por favor completa todos los campos");
   }
